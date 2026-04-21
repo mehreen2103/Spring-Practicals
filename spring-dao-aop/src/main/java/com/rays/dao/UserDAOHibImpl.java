17948008a277463baed4041bdd4a3e0eb83e2605
mehreen2103 <mehreenansari2097@gmail.com>
2026-04-21 18:51:24 +0530

    done

---
+++ spring-dao-aop/src/main/java/com/rays/dao/UserDAOHibImpl.java
@@ -0,0 +1,97 @@
+package com.rays.dao;
+
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.rays.dto.UserDTO;
+
+@Repository
+public class UserDAOHibImpl implements UserDAOInt{
+
+	@Autowired
+	private SessionFactory sessionfactory = null;
+	
+	public long add(UserDTO dto) {
+		long pk =(Long)sessionfactory.getCurrentSession().save(dto);
+		return pk;
+	}
+
+	public void update(UserDTO dto) {
+		sessionfactory.getCurrentSession().update(dto);
+	}
+
+	public UserDTO delete(long id) {
+		UserDTO dto = findByPk(id);
+		sessionfactory.getCurrentSession().delete(dto);
+		return dto;
+	}
+
+	public UserDTO findByLogin(String login) {
+		UserDTO dto = null;
+		Session session = sessionfactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(UserDTO.class);
+		criteria.add(Restrictions.eq("login", login));
+		List list = criteria.list();
+		if (list.size() == 1) {
+			dto = (UserDTO) list.get(0);
+		}
+		return dto;
+	}
+
+	public UserDTO auth(String login, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserDTO findByPk(long pk) {
+		UserDTO dto = null;
+		dto = (UserDTO) sessionfactory.getCurrentSession().get(UserDTO.class, pk);
+		return dto;
+	}
+
+	public List search(UserDTO dto) {
+		return search(dto, 0, 0);
+	}
+
+	public List search(UserDTO dto, int pageNo, int pageSize) {
+		List list = null;
+		Session session = sessionfactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(UserDTO.class);
+
+		if (dto != null) {
+			if (dto.getId() > 0) {
+				criteria.add(Restrictions.eq("id", dto.getId()));
+			}
+			if (dto.getFirstName() != null && dto.getFirstName().length() > 0) {
+				criteria.add(Restrictions.like("firstName", dto.getFirstName() + "%"));
+			}
+			if (dto.getLastName() != null && dto.getLastName().length() > 0) {
+				criteria.add(Restrictions.like("lastName", dto.getLastName() + "%"));
+			}
+			if (dto.getLogin() != null && dto.getLogin().length() > 0) {
+				criteria.add(Restrictions.like("login", dto.getLogin()));
+			}
+			if (dto.getPassword() != null && dto.getPassword().length() > 0) {
+				criteria.add(Restrictions.like("password", dto.getPassword()));
+			}
+		}
+
+		if (pageSize > 0) {
+			criteria.setFirstResult((pageNo - 1) * pageSize);
+			criteria.setMaxResults(pageSize);
+		}
+
+		list = criteria.list();
+
+		return list;
+
+	}
+
+	
+	}
+
